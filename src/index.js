cb83651d2eb591abb9f0bee11e9d49e8901ad6d2
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-04-24 10:03:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}></PersistGate>
-      <BrowserRouter basename="/so_yummy_react_project">
+      <BrowserRouter basename="/react_it_generation_AuroraPolaire">
         <App />
       </BrowserRouter>
     </Provider>
